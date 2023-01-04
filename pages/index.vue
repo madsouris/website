3ae8f1c8eeffa408bf8c2f999dae39418a43b88f
madsouris <service@vannrith.com>
2023-01-05 00:09:55 +0700

    removed spotlightjs

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,47 @@
 <template>
     <section class="container mx-auto">
-        <div class="flex flex-col md:flex-row gap-2 sm:flex-wrap md:flex-nowrap">
-            <div class="px-2 pb-2 pt-16 border border-gray-200 dark:border-gray-900 sm:basis-full md:basis-1/2">
-                <h1 class="text-7xl text-black dark:text-white tracking-tight font-bold font-sans mb-2">
+        <div
+            class="flex flex-col md:flex-row gap-2 sm:flex-wrap md:flex-nowrap">
+            <div
+                class="px-2 pb-2 pt-16 border border-gray-200 dark:border-gray-900 sm:basis-full md:basis-1/2">
+                <h1
+                    class="text-7xl text-black dark:text-white tracking-tight font-bold font-sans mb-2">
                     welcome.
                 </h1>
-                <p class="font-sans text-black dark:text-white tracking-tight mb-4">
+                <p
+                    class="font-sans text-black dark:text-white tracking-tight mb-4">
                     I am a product and graphic designer currently working in
                     Phnom Penh, Cambodia. Clear big bold designs satisfied me.
                 </p>
-                <NuxtLink to="/about"
+                <NuxtLink
+                    to="/about"
                     class="text-white dark:text-black font-bold tracking-tight font-sans no-underline py-3 px-5 bg-black dark:bg-white inline-block hover:bg-orange-500 transition transition-all">
-                    about me.</NuxtLink>
+                    about me.</NuxtLink
+                >
             </div>
             <div
                 class="p-2 border border-gray-200 dark:border-gray-900 sm:basis-full md:basis-1/2 flex flex-col justify-end">
                 <ul class="p-0 m-0 list-none">
                     <li class="mt-2">
-                        <NuxtLink to="/projects"
+                        <NuxtLink
+                            to="/projects"
                             class="font-sans font-bold text-black dark:text-white no-underline tracking-tight hover:text-orange-500 hover:underline">
                             projects.
                         </NuxtLink>
                     </li>
                     <li class="mt-2">
-                        <NuxtLink to="/about"
+                        <NuxtLink
+                            to="/about"
                             class="font-sans font-bold text-black dark:text-white no-underline tracking-tight hover:text-orange-500 hover:underline">
-                            about me.</NuxtLink>
+                            about me.</NuxtLink
+                        >
                     </li>
                     <li class="mt-2">
-                        <NuxtLink to="/blog"
+                        <NuxtLink
+                            to="/blog"
                             class="font-sans font-bold text-black dark:text-white no-underline tracking-tight hover:text-orange-500 hover:underline">
-                            blog.</NuxtLink>
+                            blog.</NuxtLink
+                        >
                     </li>
                 </ul>
             </div>
@@ -45,29 +56,44 @@
                     projects overview
                 </h2>
                 <p class="mb-2">click to view full size</p>
-                <NuxtLink to="/projects"
+                <NuxtLink
+                    to="/projects"
                     class="font-sans tracking-tight no-underline hover:underline text-orange-500 transition-all">
                     all projects
-                    <Icon name="material-symbols:arrow-right-alt-rounded" size="18px"></Icon>
+                    <Icon
+                        name="material-symbols:arrow-right-alt-rounded"
+                        size="18px"></Icon>
                 </NuxtLink>
             </div>
             <div class="basis-full md:basis-2/3">
                 <div class="grid grid-cols-2 gap-2">
                     <div class="h-full aspect-video">
-                        <NuxtImg src="/img/hangme.png" alt="Hangme" loading="lazy"
-                            class=" w-full h-full object-cover object-center cursor-pointer hover:opacity-90" />
-                    </div>
-                    <div class="h-full aspect-video">
-                        <NuxtImg src="/img/meta.png" alt="meta" loading="lazy"
-                            class=" w-full h-full object-cover object-center cursor-pointer hover:opacity-90" />
-                    </div>
-                    <div class="h-full aspect-video">
-                        <NuxtImg src="/img/startpage.png" alt="startpage" loading="lazy"
+                        <NuxtImg
+                            src="/img/hangme.png"
+                            alt="Hangme"
+                            loading="lazy"
                             class="w-full h-full object-cover object-center cursor-pointer hover:opacity-90" />
                     </div>
                     <div class="h-full aspect-video">
-                        <NuxtImg src="/img/ronin.png" alt="ronin" loading="lazy"
-                            class=" w-full h-full object-cover object-center cursor-pointer hover:opacity-90" />
+                        <NuxtImg
+                            src="/img/meta.png"
+                            alt="meta"
+                            loading="lazy"
+                            class="w-full h-full object-cover object-center cursor-pointer hover:opacity-90" />
+                    </div>
+                    <div class="h-full aspect-video">
+                        <NuxtImg
+                            src="/img/startpage.png"
+                            alt="startpage"
+                            loading="lazy"
+                            class="w-full h-full object-cover object-center cursor-pointer hover:opacity-90" />
+                    </div>
+                    <div class="h-full aspect-video">
+                        <NuxtImg
+                            src="/img/ronin.png"
+                            alt="ronin"
+                            loading="lazy"
+                            class="w-full h-full object-cover object-center cursor-pointer hover:opacity-90" />
                     </div>
                 </div>
             </div>
