8c1257ca1f0037e8f6db9648bcdb0895572f7cee
madsouris <service@vannrith.com>
2023-01-11 16:14:26 +0700

    updated footer with source code

--- components/NavigationFoot.vue
+++ components/NavigationFoot.vue
@@ -1,17 +1,15 @@
 <template>
     <footer class="flex flex-col md:flex-row flex-wrap gap-2 my-2">
-        <section
-            class="p-2 flex-1 md:flex-0 border border-gray-200 dark:border-gray-900">
-            <p
-                class="font-sans text-sm tracking-tight text-black dark:text-white text-left">
+        <section class="p-2 flex-1 md:flex-0 border border-gray-200 dark:border-gray-900">
+            <p class="font-sans text-sm tracking-tight text-black dark:text-white text-left">
                 &copy; copyright 2021 - present, all right reserved
             </p>
         </section>
-        <section
-            class="p-2 flex-1 md:flex-0 border border-gray-200 dark:border-gray-900">
-            <p
-                class="font-sans text-sm tracking-tight text-black dark:text-white text-left md:text-right">
-                powered by Nuxt, Windi and Netlify
+        <section class="p-2 flex-1 md:flex-0 border border-gray-200 dark:border-gray-900">
+            <p class="font-sans text-sm tracking-tight text-black dark:text-white text-left md:text-right">
+                powered by Nuxt, Windi and Netlify, <a class="text-orange-500 font-bold"
+                    href="//github.com/madsouris/website" target="_blank">source
+                    code</a>
             </p>
         </section>
     </footer>
