a7c3b0247755d8c0e3a51a5caa51ea41be43790e
madsouris <service@vannrith.com>
2023-03-28 15:24:36 +0700

    added gallery page

--- components/NavigationTop.vue
+++ components/NavigationTop.vue
@@ -2,12 +2,16 @@
     <header class="sticky top-0 left-0 w-full bg-amber-50 dark:bg-black z-40">
         <section
             class="flex flex-row w-full justify-between items-center border border-gray-200 dark:border-gray-900 my-2">
-            <p class="text-sm text-black dark:text-white font-sans tracking-tight p-2">
+            <p
+                class="text-sm text-black dark:text-white font-sans tracking-tight p-2">
                 Welcome
             </p>
             <ul class="list-none flex flex-row items-center m-0 p-0 gap-2 px-2">
                 <li v-for="link in links">
-                    <a :href="link.url" target="_blank" aria-label="link.name"
+                    <a
+                        :href="link.url"
+                        target="_blank"
+                        aria-label="link.name"
                         class="text-black dark:text-white hover:text-orange-500 transition transition-all">
                         <Icon :name="link.icon" size="16px" />
                     </a>
@@ -17,11 +21,17 @@
 
         <section
             class="flex flex-row w-full justify-between items-center border border-gray-200 dark:border-gray-900 mb-2">
-            <a href="/" class="p-2 border-r border-gray-200 dark:border-gray-900" aria-label="logo">
-                <div class="w-10 h-10 bg-orange-500 hover:bg-black dark:hover:bg-orange-700"></div>
+            <a
+                href="/"
+                class="p-2 border-r border-gray-200 dark:border-gray-900"
+                aria-label="logo">
+                <div
+                    class="w-10 h-10 bg-orange-500 hover:bg-black dark:hover:bg-orange-700"></div>
             </a>
             <div class="p-2 border-l border-gray-200 dark:border-gray-900">
-                <button type="button" aria-label="menu"
+                <button
+                    type="button"
+                    aria-label="menu"
                     class="text-2xl text-black dark:text-white hover:text-orange-500 border-0 bg-transparent outline-0 cursor-pointer p-0"
                     @click="this.menu = !this.menu">
                     <Icon name="ic:round-menu" size="32px" />
@@ -33,16 +43,20 @@
             class="w-full h-screen fixed top-0 left-0 bg-yellow-50 dark:bg-black z-50 flex justify-center items-center"
             v-if="menu">
             <div class="container mx-auto text-center lg:text-right">
-                <ul class="w-full list-none px-2 py-10 m-0 flex flex-col gap-10 items-center lg:items-end">
+                <ul
+                    class="w-full list-none px-2 py-10 m-0 flex flex-col gap-10 items-center lg:items-end">
                     <li v-for="url in urls">
-                        <NuxtLink :to="url.path" @click="this.menu = !this.menu"
+                        <NuxtLink
+                            :to="url.path"
+                            @click="this.menu = !this.menu"
                             class="text-black dark:text-white text-5xl md:text-7xl hover:text-orange-500 no-underline hover:underline font-sans font-bold tracking-tight transition transition-all">
                             {{ url.title }}.
                         </NuxtLink>
                     </li>
                 </ul>
 
-                <button @click="this.menu = !this.menu"
+                <button
+                    @click="this.menu = !this.menu"
                     class="text-2xl text-orange-500 border-0 bg-transparent outline-0 cursor-pointer p-0 mt-6 md:mt-10">
                     <Icon name="mdi:close-thick" size="48px" />
                 </button>
@@ -73,27 +87,31 @@ export default defineComponent({
                     title: 'blog',
                     path: '/blog',
                 },
+                gallery: {
+                    title: 'gallery',
+                    path: '/gallery',
+                },
             },
             links: {
                 dribbble: {
                     url: 'https://dribbble.com/madsouris',
                     icon: 'bxl:dribbble',
-                    name: 'dribbble'
+                    name: 'dribbble',
                 },
                 linkedin: {
                     url: 'https://linkedin.com/in/madsouris',
                     icon: 'ri:linkedin-fill',
-                    name: 'linkedin'
+                    name: 'linkedin',
                 },
                 github: {
                     url: 'https://github.com/madsouris',
                     icon: 'mdi:github',
-                    name: 'github'
+                    name: 'github',
                 },
                 mail: {
                     url: 'mailto:me@vannrith.com',
                     icon: 'material-symbols:mail-outline-rounded',
-                    name: 'mail'
+                    name: 'mail',
                 },
             },
         }
---
+++ pages/gallery.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="flex flex-col md:flex-row gap-2 mb-2 items-start">
+        <div
+            class="border border-gray-200 dark:border-gray-900 p-2 basis-full md:basis-1/3 lg:basis-1/4 md:sticky top-[112px]">
+            <img
+                src="/img/me600.jpg"
+                loading="lazy"
+                alt="my photo"
+                class="w-full" />
+            <div class="flex flex-col gap-2 mt-2">
+                <h1
+                    class="text-2xl tracking-tight text-black dark:text-white font-bold font-sans mb-2">
+                    Vannrith (វណ្ណរិទ្ធ) AKA. Souris
+                </h1>
+            </div>
+        </div>
+        <div class="basis-full md:basis-2/3 lg:basis-3/4">
+            <div
+                class="flex flex-col gap-2 border border-gray-200 dark:border-gray-900 p-2">
+                <h1
+                    class="text-7xl text-black dark:text-white tracking-tight font-bold font-sans">
+                    Gallery
+                </h1>
+                <p>
+                    My name is Vannrith Va, mostly known as Souris. I am a
+                    product designer and front-end developer working from Phnom
+                    Penh, Cambodia.
+                </p>
+            </div>
+            <div
+                class="flex flex-col gap-2 border border-gray-200 dark:border-gray-900 p-2 mt-2">
+                <h1
+                    class="text-2xl tracking-tight text-black dark:text-white font-bold font-sans">
+                    about this website.
+                </h1>
+                <p>
+                    This website was built with Nuxt 3, and some other goodies
+                    like Windi CSS and Netlify.
+                </p>
+            </div>
+        </div>
+    </div>
+    <CallToAction />
+</template>
+
+<style scoped>
+.embed iframe html body {
+    background: black !important;
+}
+</style>
+
+<script setup lang="ts">
+useHead({
+    title: 'About | Souris | Product Designer',
+    meta: [
+        {
+            property: 'twitter:title',
+            content: 'About | Souris | Product Designer',
+        },
+
+        {
+            property: 'og:title',
+            content: 'About | Souris | Product Designer',
+        },
+        {
+            name: 'description',
+            content:
+                'I do love minimal design. Being a Cambodian, surrounded by busy-designs, inspired me to make something different, design to solve issue instead of making it look a certain way.',
+        },
+
+        {
+            property: 'og:description',
+            content:
+                'I do love minimal design. Being a Cambodian, surrounded by busy-designs, inspired me to make something different, design to solve issue instead of making it look a certain way.',
+        },
+        {
+            property: 'twitter:description',
+            content:
+                'I do love minimal design. Being a Cambodian, surrounded by busy-designs, inspired me to make something different, design to solve issue instead of making it look a certain way.',
+        },
+        {
+            property: 'og:type',
+            content: 'website',
+        },
+        {
+            property: 'og:url',
+            content: 'https://vannrith.com/about',
+        },
+        {
+            property: 'og:image',
+            content: '/img/feature.jpg',
+        },
+        {
+            property: 'twitter:card',
+            content: 'summary_large_image',
+        },
+        {
+            property: 'twitter:url',
+            content: 'https://vannrith.com/about',
+        },
+        {
+            property: 'twitter:image',
+            content: '/img/feature.jpg',
+        },
+    ],
+})
+</script>
+
+<script lang="ts">
+export default defineComponent({
+    data() {
+        return {}
+    },
+})
+</script>
