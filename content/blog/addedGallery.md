eb357538e17e6075527a497ebbc6146ae620ea4c
madsouris <service@vannrith.com>
2023-04-19 14:05:48 +0700

    #12 #16 finished all opened issues

---
+++ content/blog/addedGallery.md
@@ -0,0 +1,44 @@
+---
+title: 'New Gallery Page'
+description: 'I just added gallery page'
+category: 'Talk'
+author: 'Souris'
+img: '/img/feature.jpg'
+head:
+    {
+        meta:
+            [
+                { property: 'title', content: 'New Gallery Page | Souris' },
+                { property: 'og:title', content: 'New Gallery Page | Souris' },
+                {
+                    property: 'og:description',
+                    content: 'I just added gallery page',
+                },
+                { property: 'og:image', content: '/img/ronin.png' },
+                { property: 'twitter:image', content: '/img/ronin.png' },
+                {
+                    property: 'twitter:title',
+                    content: 'New Gallery Page | Souris',
+                },
+                {
+                    property: 'twitter:description',
+                    content: 'I just added gallery page',
+                },
+                { proterty: 'keywords', content: 'blog,gallery' },
+            ],
+    }
+---
+
+After a long time, I finally managed to create a gallery page.
+
+<!--more-->
+
+# New page
+
+I feel I should make more content for my own website, and one aspect of it is a gallery page. Right now it is divided into 2 categories, graphic design and UI design.
+
+# A window for better websites
+
+For image hosting, I go with Imagekit, because it has a great free plan to start and natively support in Nuxt Image v1. Combined with Spotlight.js, the images load quick and can be enlarge.
+
+With all of these works done, I feel confident to build a gallery website for other people without worrying about speed.
--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,15 +1,63 @@
 <template>
     <div class="flex flex-col md:flex-row gap-2 mb-2 items-start">
         <div class="basis-full">
-            <div class="flex flex-col gap-2 border border-gray-200 dark:border-gray-900 px-2 py-12 text-center">
-                <h1 class="text-7xl text-black dark:text-white tracking-tight font-bold font-sans">
-                    Gallery
+            <div
+                class="flex flex-col gap-2 border border-gray-200 dark:border-gray-900 p2 text-center">
+                <h1
+                    class="text-7xl text-black dark:text-white tracking-tight font-bold font-sans">
+                    gallery.
                 </h1>
                 <p>
-                    Here are some of my works I've done over the years. <br />
-                    A lot of them I can't really show because they are the
+                    here are some of my works I've done over the years. <br />
+                    a lot of them I can't really show because they are the
                     properties of my previous companies.
                 </p>
+                <h2
+                    class="text-5xl text-black dark:text-white tracking-tight font-bold font-sans mt-8">
+                    user interface designs
+                </h2>
+                <p class="mb-2">click to enlarge</p>
+                <div
+                    class="w-full p-2 flex flex-row flex-wrap justify-center spotlight-group">
+                    <a
+                        v-for="i in 33"
+                        :key="i"
+                        :href="
+                            'https://ik.imagekit.io/madsouris/vannrith/ui/' +
+                            i +
+                            '.jpg?tr=w-1200'
+                        "
+                        class="spotlight basis-1/3 md:basis-1/6 lg:basis-1/9 hover:opacity-90">
+                        <nuxt-img
+                            provider="imagekit"
+                            :src="'/vannrith/ui/' + i + '.jpg'"
+                            width="320"
+                            height="320" />
+                    </a>
+                </div>
+                <h2
+                    class="text-5xl text-black dark:text-white tracking-tight font-bold font-sans mt-8">
+                    graphic designs
+                </h2>
+                <p class="mb-2">click to enlarge</p>
+                <div
+                    class="w-full p-2 flex flex-row flex-wrap justify-center spotlight-group">
+                    <a
+                        v-for="i in 19"
+                        :key="i"
+                        :href="
+                            'https://ik.imagekit.io/madsouris/vannrith/graphic/' +
+                            i +
+                            '.jpg?tr=w-1200'
+                        "
+                        class="spotlight basis-1/3 md:basis-1/6 lg:basis-1/9 hover:opacity-90">
+                        <nuxt-img
+                            provider="imagekit"
+                            :src="'/vannrith/graphic/' + i + '.jpg'"
+                            width="320"
+                            height="320" />
+                    </a>
+                </div>
             </div>
         </div>
     </div>
@@ -24,33 +72,32 @@
 
 <script setup lang="ts">
 useHead({
-
-    title: 'About | Souris | Product Designer',
+    title: 'Gallery | Souris | Product Designer',
     meta: [
         {
             property: 'twitter:title',
-            content: 'About | Souris | Product Designer',
+            content: 'Gallery | Souris | Product Designer',
         },
 
         {
             property: 'og:title',
-            content: 'About | Souris | Product Designer',
+            content: 'Gallery | Souris | Product Designer',
         },
         {
             name: 'description',
             content:
-                'I do love minimal design. Being a Cambodian, surrounded by busy-designs, inspired me to make something different, design to solve issue instead of making it look a certain way.',
+                'Some of my previous works in gallery format. While I did a lot more since 2015, majority of them belong to my previous companies.',
         },
 
         {
             property: 'og:description',
             content:
-                'I do love minimal design. Being a Cambodian, surrounded by busy-designs, inspired me to make something different, design to solve issue instead of making it look a certain way.',
+                'Some of my previous works in gallery format. While I did a lot more since 2015, majority of them belong to my previous companies.',
         },
         {
             property: 'twitter:description',
             content:
-                'I do love minimal design. Being a Cambodian, surrounded by busy-designs, inspired me to make something different, design to solve issue instead of making it look a certain way.',
+                'Some of my previous works in gallery format. While I did a lot more since 2015, majority of them belong to my previous companies.',
         },
         {
             property: 'og:type',
@@ -58,7 +105,7 @@ useHead({
         },
         {
             property: 'og:url',
-            content: 'https://vannrith.com/about',
+            content: 'https://vannrith.com/gallery',
         },
         {
             property: 'og:image',
@@ -70,7 +117,7 @@ useHead({
         },
         {
             property: 'twitter:url',
-            content: 'https://vannrith.com/about',
+            content: 'https://vannrith.com/gallery',
         },
         {
             property: 'twitter:image',
