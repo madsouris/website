d9952bde8db30da08aa61aa9064fcf73b9f649f1
madsouris <service@vannrith.com>
2024-08-06 16:00:46 +0700

    added images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import alpinejs from "@astrojs/alpinejs";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import alpinejs from '@astrojs/alpinejs'
 
-import icon from "astro-icon";
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), icon()]
-});
+    integrations: [tailwind(), alpinejs(), icon()],
+    image: {
+        domains: ['ik.imagekit.io'],
+        remotePatterns: [{ protocol: 'https' }],
+    },
+})
